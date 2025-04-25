587acac6f0d074ce1bd6b9d82745ef7bf4372c42
Anastasia <nastya.gudoshnikova@list.ru>
2025-04-25 23:35:09 +0400

    это конец

---
+++ src/main/java/com/example/backendNeoflexWind/dto/UserAchievementDto.java
@@ -0,0 +1,2 @@
+package com.example.backendNeoflexWind.dto;public class UserAchievementDto {
+}
---
+++ src/main/java/com/example/backendNeoflexWind/models/AchievementUnlockResponse.java
@@ -0,0 +1,2 @@
+package com.example.backendNeoflexWind.models;public class AchievementUnlockResponse {
+}
